b2aa158530c80c7ca4b8e56bab6439b49d6e107c
Oziel Mancilla Diaz <oziel.mancilla@hotmail.com>
2021-06-08 17:02:23 -0500

    tarea 2 ok

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace dev_homework03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("********************");
+            /* Console.WriteLine("********************");
             Console.WriteLine("Tarea - 01");
             Console.WriteLine("Simple Array Sum");
 
@@ -36,8 +36,44 @@ namespace dev_homework03
             Console.WriteLine("El valor total es: " + suma);
 
             
+            Console.WriteLine("********************"); */
+            Console.WriteLine("\n");
             Console.WriteLine("********************");
+            Console.WriteLine("Tarea - 02");
+            Console.WriteLine("Simple Array Sum");
             
+            var positiveNumbers = 0;
+            var negativeNumbers = 0;
+            var zeroNumbers = 0;
+            //No need to capture the size of array. I use array's length property instead.
+            Console.WriteLine("¿Cúantos numeros vas a introducir?");
+           
+            Console.ReadLine();
+            Console.WriteLine("Introduce los numeros, pueden ser postivos, negativos o cero." );
+             var arr_temp = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            for (int arr_i = 0; arr_i < arr.Length; arr_i++)
+            {
+            if (arr[arr_i] > 0)
+                ++positiveNumbers;
+            else if (arr[arr_i] < 0)
+                ++negativeNumbers;
+            else
+                ++zeroNumbers;
+            }
+            Console.WriteLine("hay un ");
+            Console.Write((double)positiveNumbers / arr.Length);
+            Console.WriteLine("% de nuemros positivos ");
+            Console.Write((double)negativeNumbers / arr.Length);
+            Console.WriteLine("% de nuemros negativos ");
+            Console.Write((double)zeroNumbers / arr.Length);
+            Console.WriteLine("% de ceros ");
+
+
+
+
+
         }
     }
 }
