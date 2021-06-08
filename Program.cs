48a65a289a45ca18d60bd874192d74711bda0801
ozii-el <oziel.mancilla@hotmail.com>
2021-06-07 19:47:02 -0500

    Tarea 1 Done

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace dev_homework03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("Tarea - 01");
+            Console.WriteLine("Simple Array Sum");
+
+
+            Console.WriteLine("Introduce cuantos numeros quieres sumar:");
+            int numero=0;
+            int suma = 0;
+            
+            numero=Convert.ToInt32(Console.ReadLine());
+            string [] lectura = new string [numero];
+            var arreglo= new int[numero];
+            Console.Write("Introduce ");
+            Console.Write(numero);
+            Console.WriteLine(" numeros");
+            
+            lectura = Console.ReadLine().TrimEnd().Split(" ");
+
+
+             if (lectura.Length>numero){
+                Console.WriteLine("Favor de verificar la cantidad de numeros introducidos");
+            } else{
+                for(int i=0; i < lectura.Length;i++){
+                    suma = suma+Convert.ToInt32(lectura[i]);
+                }
+            }
+
+            Console.WriteLine("El valor total es: " + suma);
+
+            
+            Console.WriteLine("********************");
+            
+        }
+    }
+}
